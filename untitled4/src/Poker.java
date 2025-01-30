5b8c6430f1521b0ac01c6d32df3eb43394b9cafb
ThatOneWriter <168831146+ThatOneWriter@users.noreply.github.com>
2025-01-30 10:55:06 -0500

    Update Poker.java
    1/30/25 - currently working on part 1 and 2

--- untitled4/src/Poker.java
+++ untitled4/src/Poker.java
@@ -1,28 +1,98 @@
-import java.io.File;
-import java.util.Arrays;
-
-
 public class Poker {
 
-    public static void main(String[] args) {
 
-        String[] poker = {"3","2","10","3", "King"};
 
-        File f = new File("src/input");
+    public static void main(String[] args)
+    {
+        String[] poker = new String[]{"Number of five of a kind hands: " + fiveOfAKind() + "\n",
+                "Number of full house hands: " + fullHouse() + "\n",
+                "Number of four of a kind hands: " + fourOfAKind() + "\n",
+                "Number of three of a kind hands: " + threeOfAKind() + "\n",
+                "Number of two pair hands: " + twoPair() + "\n",
+                "Number of one pair hands: " + onePair() + "\n",
+                "Number of high card hands: " + highCard() + "\n",
+                "Total Bid Value: " + "\n",
+                "Total Bid Value With Jacks Wild: " + "\n"};
 
-        String fileData = "";
 
+        System.out.println(Arrays.toString(poker));
     }
 
-    public static void ifJack() {
 
+
+    public static int fiveOfAKind() {
+
+        return 1;
     }
 
-    public static void getRank(String[] rank) {
-
+    public static int fourOfAKind() {
+        return 1;
     }
 
-    public static void getHand(String[] hand) {
+    public static int fullHouse() {
+        return 1;
+    }
+
+    public static int threeOfAKind() {
+        return 1;
+    }
+
+    public static int twoPair() {
+        return 1;
+    }
+
+    public static int onePair() {
+        return 1;
+    }
+
+
+    public static int highCard() {
+        return 1;
+    }
+
+    public static int getRank(int numOfCards, boolean card) {
+        int a = 0;
+        if (5 != 6) {
+            numOfCards++;
+            a += numOfCards;
+            return a;
+        }
+
+
+        if (4 == 5) {
+            numOfCards++;
+            a += numOfCards;
+            return a;
+        }
+
+
+        if (3 == 4) {
+            numOfCards++;
+            a += numOfCards;
+            return a;
+        }
+
+        if (2 == 3) {
+            numOfCards++;
+            a += numOfCards;
+            return a;
+        }
+
+        if (1 == 2) {
+            numOfCards++;
+            a += numOfCards;
+            return a;
+        }
+
+        if (card) {
+            numOfCards++;
+            a += numOfCards;
+            return a;
+        }
+        return a;
+    }
+    public static void getHand() {
+
 
     }
 
@@ -40,9 +110,7 @@ public class Poker {
 
     }
 
-    @Override
-    public String toString() {
-        return super.toString();
-    }
+
+
 }
 
